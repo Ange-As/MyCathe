3436101efae9dd3254eecd51ccfd5bc6be69e7a7
ODC_USER <odc_user@iMac-de-ODC-USER.local>
2024-08-20 11:38:12 +0000

    new maj

--- app-frontend/components/CardGetDetailProfil.vue
+++ app-frontend/components/CardGetDetailProfil.vue
@@ -2,14 +2,18 @@
 const rows = ref<any>([]);
 const route = useRoute();
 const catechumeneStore = useCatechumeneStore();
+const isLoading = ref(false);
 
 async function getDetail() {
+  isLoading.value = true;
   try {
     const response = await catechumeneStore.getInfoCate(route.params.matricule);
     rows.value = response;
     console.log(rows);
   } catch (error) {
     console.log(error);
+  } finally {
+    isLoading.value = false;
   }
 }
 
@@ -24,27 +28,34 @@ onMounted(() => {
       <h1 class="text-xl">Vos informations</h1>
     </template>
 
-    <div class="space-y-5" v-if="rows.length != 0">
-      <p>Nom: {{ rows.nom }}</p>
-      <p>Prenom: {{ rows.prenom }}</p>
-      <!-- <p>Nom: {{ rows.nom }}</p> -->
+    <div v-if="!isLoading">
+      <div class="space-y-5" v-if="rows.length != 0">
+        <p>Nom: {{ rows.nom }}</p>
+        <p>Prenom: {{ rows.prenom }}</p>
+        <!-- <p>Nom: {{ rows.nom }}</p> -->
 
-      <UButton
-        label="Passer au paiement"
-        block
-        size="lg"
-        @click="navigateTo(`/catechumene/paiement/${rows.id}`)"
-      />
+        <UButton
+          label="Passer au paiement"
+          block
+          size="lg"
+          @click="navigateTo(`/catechumene/paiement/${rows.id}`)"
+        />
+      </div>
+
+      <div v-else>
+        <p class="text-red-500 mb-5 text-center">Ce compte n'existe pas !</p>
+        <UButton
+          label="Inscrivez-vous"
+          block
+          size="lg"
+          @click="
+            navigateTo(`/catechumene/${route.params.tranche}/choices/new/`)
+          "
+        />
+      </div>
     </div>
-
-    <div v-else>
-      <p class="text-red-500 mb-5 text-center">Votre compte n'existe pas !</p>
-      <UButton
-        label="Inscrivez-vous"
-        block
-        size="lg"
-        @click="navigateTo(`/catechumene/${route.params.tranche}/choices/new/`)"
-      />
+    <div v-else class="flex justify-center items-end">
+      <UProgress animation="carousel" size="sm" />
     </div>
   </UCard>
 </template>
